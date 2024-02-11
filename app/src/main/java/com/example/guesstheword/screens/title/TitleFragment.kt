73c723a347c18e8ecf037a85a3840f2151b11a98
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-11 16:50:46 +0700

    feat: add title fragment

---
+++ app/src/main/java/com/example/guesstheword/screens/title/TitleFragment.kt
@@ -0,0 +1,25 @@
+package com.example.guesstheword.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.guesstheword.R
+import com.example.guesstheword.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        // Button navigation action
+
+        return binding.root
+    }
+}
